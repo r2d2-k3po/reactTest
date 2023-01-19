ad9dab62048c4f29e1f256261f706dea7350e1fb
r2d2-k3po <r2d2factory3@gmail.com>
2023-01-19 19:24:42 +0900

    insert remove toggle

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,7 @@
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
-import {useState} from "react";
+import {useState, useRef, useCallback} from "react";
 
 const App = () => {
     const [todos, setTodos] = useState([
@@ -22,10 +22,43 @@ const App = () => {
         },
     ]);
 
+    const nextId = useRef(4);
+
+    const onInsert = useCallback(
+        text => {
+            const todo = {
+                id: nextId.current,
+                text,
+                checked: false,
+            };
+            setTodos(todos.concat(todo));
+            nextId.current += 1;
+        },
+        [todos],
+    );
+
+    const onRemove = useCallback(
+        id => {
+            setTodos(todos.filter(todo => todo.id !== id));
+        },
+        [todos],
+    );
+
+    const onToggle = useCallback(
+        id => {
+            setTodos(
+                todos.map(todo =>
+                    todo.id === id ? {...todo, checked: !todo.checked} : todo,
+                ),
+            );
+        },
+        [todos],
+    );
+
   return (
       <TodoTemplate>
-        <TodoInsert />
-        <TodoList todos={todos} />
+        <TodoInsert onInsert={onInsert} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
       </TodoTemplate>
   );
 };
