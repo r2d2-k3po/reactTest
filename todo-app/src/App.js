3c85de4b90bce7b3cf9003b43b62780f18b26c80
r2d2-k3po <r2d2factory3@gmail.com>
2023-01-19 10:44:06 +0900

    todos useState

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,31 @@
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
+import {useState} from "react";
 
 const App = () => {
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            text: '리액트의 기초 알아보기',
+            checked: true,
+        },
+        {
+            id: 2,
+            text: '컴포넌트 스타일링해 보기',
+            checked: true,
+        },
+        {
+            id: 3,
+            text: '일정 관리 앱 만들어 보기',
+            checked: false,
+        },
+    ]);
+
   return (
       <TodoTemplate>
-        <TodoInsert/>
+        <TodoInsert />
+        <TodoList todos={todos} />
       </TodoTemplate>
   );
 };
