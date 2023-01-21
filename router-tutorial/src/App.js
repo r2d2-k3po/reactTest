60c7ddc26654bc5240b95e55ce3cbb1ed77f0e2c
r2d2-k3po <r2d2factory3@gmail.com>
2023-01-21 23:28:43 +0900

    router

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,24 @@
+import {Route, Routes} from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
+import Layout from "./Layout";
+
+const App = () => {
+  return (
+    <Routes>
+        <Route element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/profiles/:username" element={<Profile />} />
+        </Route>
+        <Route path="/articles" element={<Articles />} >
+            <Route path=":id" element={<Article />} />
+        </Route>
+    </Routes>
+  );
+};
+
+export default App;
