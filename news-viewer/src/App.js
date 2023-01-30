487039f7eff26234c48a397dd8e68f211b8cbb12
r2d2-k3po <r2d2factory3@gmail.com>
2023-01-30 19:59:34 +0900

    category

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,32 +1,16 @@
-import { useState } from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
+import { useCallback, useState } from 'react';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=51f8efb4ec4f4f4580e082679986af73',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
 
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
   );
 };
 
